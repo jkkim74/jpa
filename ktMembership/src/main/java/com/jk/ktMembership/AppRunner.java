a3d477096b20a4b58f8859107f8a21cd30c3791a
jack6 <jack6@DESKTOP-9052OTG.mshome.net>
2019-12-29 22:53:20 +0900

    User 정보 입력 테스트

---
+++ ktMembership/src/main/java/com/jk/ktMembership/AppRunner.java
@@ -0,0 +1,48 @@
+package com.jk.ktMembership;
+
+import javax.transaction.Transactional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+@Transactional
+public class AppRunner implements ApplicationRunner{
+	
+	@Autowired
+	UserRepository userRepository;
+
+	@Override
+	public void run(ApplicationArguments args) throws Exception {
+		// TODO Auto-generated method stub
+//		User user = new User();
+//		user.setName("jkkim");
+//		
+//		userRepository.save(user);
+//		
+//		User user2 = new User();
+//		user2.setName("eunhye");
+//		
+//		userRepository.save(user2);
+//		
+//		User user3 = new User();
+//		user3.setName("anyOne");
+//		
+//		userRepository.save(user3);
+//		
+//		User user4 = new User();
+//		user4.setName("user4");
+//		
+//		userRepository.save(user4);
+//		
+//		User user5 = new User();
+//		user5.setName("user5");
+//		
+//		userRepository.save(user5);
+		
+		
+	}
+
+}
