d2aeb3628f4442300ff5fc441adc506c9106cbc0
jack6 <jack6@DESKTOP-9052OTG.mshome.net>
2019-12-29 21:57:20 +0900

    added Repository test

---
+++ ktMembership/src/main/java/com/jk/ktMembership/LoginRepository.java
@@ -0,0 +1,9 @@
+package com.jk.ktMembership;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface LoginRepository extends JpaRepository<Login,Long>{
+
+}
